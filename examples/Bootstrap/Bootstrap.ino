f8f71306e327aab02aade28bca0f0f266da17c9e
Eric Jennings <ericj@loopshot.com>
2014-03-24 11:31:24 -0700

    Add Bootstrap to the examples directory

---
+++ examples/Bootstrap/Bootstrap.ino
@@ -0,0 +1,17 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Scout.h>
+#include <GS.h>
+#include <bitlash.h>
+#include <lwm.h>
+#include <js0n.h>
+
+void setup() {
+  Scout.setup();
+  // Add custom setup code here
+}
+
+void loop() {
+  Scout.loop();
+  // Add custom loop code here
+}
