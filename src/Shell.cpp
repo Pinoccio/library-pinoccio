c5bad602569cf65fbe74c31b9c28231594e4345e
Eric Jennings <ericj@loopshot.com>
2014-04-16 15:27:33 -0700

    Remove high/low temp since /sync has all history now

--- src/Shell.cpp
+++ src/Shell.cpp
@@ -451,9 +451,6 @@ void PinoccioShell::disableShell() {
 
 static NWK_DataReq_t sendDataReq;
 static bool sendDataReqBusy;
-static int tempHigh = 0;
-static int tempLow = 0;
-
 
 /****************************\
 *      BUILT-IN HANDLERS    *
@@ -462,16 +459,10 @@ static int tempLow = 0;
 static StringBuffer tempReportHQ(void) {
   StringBuffer report(100);
   int temp = Scout.getTemperature();
-  if(temp > tempHigh) tempHigh = temp;
-  if(!tempLow || temp < tempLow) tempLow = temp;
-  report.appendSprintf("[%d,[%d,%d,%d],[%d,%d,%d]]",
+  report.appendSprintf("[%d,[%d],[%d]]",
           keyMap("temp", 0),
           keyMap("current", 0),
-          keyMap("high", 0),
-          keyMap("low", 0),
-          temp,
-          tempHigh,
-          tempLow);
+          temp);
   return Scout.handler.report(report);
 }
 
