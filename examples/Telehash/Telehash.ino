809b62083a01702d27728a0be29815074bcdb6a7
Jeremie Miller <jeremie@jabber.org>
2014-04-20 19:10:40 -0600

    more debugging/testing framework for bridging over serial

--- examples/Telehash/Telehash.ino
+++ examples/Telehash/Telehash.ino
@@ -6,6 +6,7 @@
 #include <bitlash.h>
 #include <lwm.h>
 #include <js0n.h>
+#include <AESLib.h>
 extern "C" {
 #include <switch.h>
 #include <avr.h>
@@ -76,36 +77,64 @@ void readPacket()
   switch_receive(ths,p,from);
 }
 
+unsigned char *localBuf;
+int localBufLen = 0;
 static numvar localIn(void)
 {
-  if(!getarg(1) || !isstringarg(1))
+  if(getarg(1) && isstringarg(1))
   {
-    speol("missing arg");
-    return 0;
+    const char *str = (const char *)getarg(1);
+    int len = strlen(str)/2;
+    // buffering up mode
+    if(len)
+    {
+      unsigned char *buf = (unsigned char*)realloc(localBuf,localBufLen+len);
+      if(!buf)
+      {
+        free(localBuf);
+        speol("out of memory ");
+        speol(localBufLen+len);
+        localBufLen = 0;
+        return 0;
+      }
+      localBuf = buf;
+      util_unhex((unsigned char*)str,len*2,localBuf+localBufLen);
+      localBufLen += len;
+      sp("buffered ");
+      speol(localBufLen);
+    }
+    return 1;
   }
-  const char *str = (const char *)getarg(1);
-  int len = strlen(str)/2;
-  unsigned char *raw = (unsigned char *)malloc(len);
-  util_unhex((unsigned char*)str,len*2,raw);
+  if(!localBufLen) return 0;
+  // packet parsing
   path_t from = path_new("local");
-  packet_t p = packet_parse(raw,len);
-  free(raw);
+  packet_t p = packet_parse(localBuf,localBufLen);
+  free(localBuf);
+  localBufLen = 0;
   if(!p)
   {
     speol("invalid packet");
     return 0;
   }
-  printf("received %s packet %d\n", p->json_len?"open":"line", len);
+  speol("processing");
+  DEBUG_PRINTF("received %s packet %d", p->json_len?(p->json_len>1?packet_get_str(p,"type"):"open"):"line", packet_len(p));
   switch_receive(ths,p,from);
   return 1;
 }
 
 void localWrite(packet_t p)
 {
-  sp("telehash:");
   unsigned char *hex = (unsigned char *)malloc(packet_len(p)*2+1);
-  speol((char*)util_hex(packet_raw(p),packet_len(p),hex));
-  free(hex);
+  if(!hex)
+  {
+    sp("no memory ");
+    speol(packet_len(p)*2+1);
+  }else{
+    sp("\ntelehash:");
+    speol((char*)util_hex(packet_raw(p),packet_len(p),hex));    
+    free(hex);
+  }
+  packet_free(p);
 }
 
 // check for outgoing packets
@@ -114,13 +143,17 @@ void sendLoop(void)
   packet_t p;
   while((p = switch_sending(ths)))
   {
-    if(util_cmp(p->out->type,"ipv4")==0 && isOn)
+    DEBUG_PRINTF("sending %s %s packet %d %s",p->out->type,p->json_len?"open":"line",packet_len(p),path_json(p->out));
+    if(util_cmp(p->out->type,"ipv4") == 0 && isOn)
     {
-      DEBUG_PRINTF("sending ipv4 %s packet %d %s\n",p->json_len?"open":"line",packet_len(p),path_json(p->out));
       writePacket(path_ip(p->out,0),path_port(p->out,0),packet_raw(p),packet_len(p));
+      packet_free(p);
+    }else if(util_cmp(p->out->type,"local") == 0){
+       localWrite(p);
+    }else{
+      DEBUG_PRINTF("%s packet %hu no network for %s",p->json_len?"open":"line",packet_len(p),path_json(p->out));
+      packet_free(p);
     }
-    if(util_cmp(p->out->type,"local")==0) localWrite(p);
-    packet_free(p);
   }
 }
 
@@ -129,11 +162,14 @@ void setup() {
   Scout.wifi.onOn = onOn;
   platform_debugging(1);
   addBitlashFunction("telehash", (bitlash_function) localIn);
-  
-  char seedjs[] = "{\"paths\":[{\"type\":\"ipv4\",\"ip\":\"192.168.0.36\",\"port\":42424}],\"parts\":{\"1a\":\"821e083c2b788c75bf4608e66a52ef2d911590f6\"},\"keys\":{\"1a\":\"z6yCAC7r5XIr6C4xdxeX7RlSmGu9Xe73L1gv8qecm4/UEZAKR5iCxA==\"}}";
-  packet_t keys = packet_new();
   crypt_init();
-  crypt_keygen(0x1a,keys);
+  
+  char keyjs[] = "{\"1a\":\"kNcFp/4mRAS7lSHjhfCfAHZv264e8nGONBuenfzJS3gSCJmHDbpy4w==\",\"1a_secret\":\"estHaS+O5xRYGyPhLcXL9Iaggfk=\"}";
+  packet_t keys = packet_new();
+//  crypt_keygen(0x1a,keys);
+  packet_json(keys,(unsigned char*)keyjs,strlen(keyjs));
+
+  char seedjs[] = "{\"paths\":[{\"type\":\"ipv4\",\"ip\":\"192.168.0.36\",\"port\":42424}],\"parts\":{\"1a\":\"821e083c2b788c75bf4608e66a52ef2d911590f6\"},\"keys\":{\"1a\":\"z6yCAC7r5XIr6C4xdxeX7RlSmGu9Xe73L1gv8qecm4/UEZAKR5iCxA==\"}}";
   ths = switch_new(41);
   if(switch_init(ths,keys)) Serial.println("switch init failed");
   DEBUG_PRINTF("loaded hashname %s",ths->id->hexname);
@@ -143,14 +179,11 @@ void setup() {
   packet_free(p);
   DEBUG_PRINTF("loaded seed %s",seed->hexname);
   
-  p = packet_new();
-  packet_set_str(p,"type","ping");
-  packet_set(p,"1a","true",4);
-  localWrite(p);
-  packet_free(p);
+  localWrite(switch_ping(ths));
 }
 
 void loop() {
   Scout.loop();
   readPacket();
+  sendLoop();
 }
