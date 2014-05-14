453e3ec563d2b31dffdf9a3e776073d7ef80eb51
Eric Jennings <ericj@loopshot.com>
2014-05-14 12:26:18 -0700

    Remove broken examples and add back a proper blink example

---
+++ examples/LEDBlink/LEDBlink.ino
@@ -0,0 +1,28 @@
+/**************************************************************************\
+* Pinoccio Library                                                         *
+* https://github.com/Pinoccio/library-pinoccio                             *
+* Copyright (c) 2014, Pinoccio Inc. All rights reserved.                   *
+* ------------------------------------------------------------------------ *
+*  This program is free software; you can redistribute it and/or modify it *
+*  under the terms of the MIT License as described in license.txt.         *
+\**************************************************************************/
+#include <SPI.h>
+#include <Wire.h>
+#include <Scout.h>
+#include <GS.h>
+#include <bitlash.h>
+#include <lwm.h>
+#include <js0n.h>
+
+#include "version.h"
+
+void setup() {
+  Scout.setup(SKETCH_NAME, SKETCH_REVISION, SKETCH_BUILD);
+  // Add custom setup code here
+  Led.blinkCyan(500, true);
+}
+
+void loop() {
+  Scout.loop();
+  // Add custom loop code here
+}
