044bde7c7b409c3118015aef52ee7ebed68ebba3
Jeremie Miller <jeremie@jabber.org>
2014-08-03 07:51:30 -0600

    make sure ordering is consistent in rebuild

--- src/modules/rebuild.js
+++ src/modules/rebuild.js
@@ -1,15 +1,18 @@
 var fs = require('fs');
 
+var dirs = [];
+fs.readdirSync('.').forEach(function(file){
+  if(fs.lstatSync(file).isDirectory()) dirs.push(file);
+});
+dirs = dirs.sort();
+
 // do the Modules.h
 var header = fs.createWriteStream('Modules.h');
 header.write('// DO NOT EDIT, auto-generated from rebuild.js\n\n');
 header.write('#include "PinoccioModule.h"\n\n');
 
-fs.readdirSync('.').forEach(function(file){
-  if(fs.lstatSync(file).isDirectory())
-  {
-    header.write('#include "'+file+'/'+file+'.h"\n');
-  } 
+dirs.forEach(function(mod){
+  header.write('#include "'+mod+'/'+mod+'.h"\n');
 });
 
 header.write('\nPinoccioModule *ModulesNamed(const char *name);\n');
@@ -23,21 +26,15 @@ cpp.write('#include <Scout.h>\n');
 cpp.write('#include "Modules.h"\n\n');
 cpp.write('PinoccioModule *ModulesNamed(const char *name)\n{\n');
 
-fs.readdirSync('.').forEach(function(file){
-  if(fs.lstatSync(file).isDirectory())
-  {
-    cpp.write('  if(strcmp("'+file.toLowerCase()+'",name) == 0) return (PinoccioModule*)(new '+file+'Module());\n');
-  } 
+dirs.forEach(function(mod){
+  cpp.write('  if(strcmp("'+mod.toLowerCase()+'",name) == 0) return (PinoccioModule*)(new '+mod+'Module());\n');
 });
 
 cpp.write('  return NULL;\n}\n\n');
 cpp.write('void ModulesPrint()\n{\n');
 
-fs.readdirSync('.').forEach(function(file){
-  if(fs.lstatSync(file).isDirectory())
-  {
-    cpp.write('  speol("'+file.toLowerCase()+'");\n');
-  } 
+dirs.forEach(function(mod){
+  cpp.write('  speol("'+mod.toLowerCase()+'");\n');
 });
 
 cpp.write('}\n');
