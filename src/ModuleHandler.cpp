b28c537d89a3b0457f178711f50970110e1cb145
Matthijs Kooijman <matthijs@stdin.nl>
2014-08-06 22:07:10 +0200

    Rename moduleList to loadedModules and move it into ModuleHandler
    Previously, it was just a global variable. Better to move it inside the
    ModuleHandler scope to prevent collisions.

--- src/ModuleHandler.cpp
+++ src/ModuleHandler.cpp
@@ -10,14 +10,14 @@
 #include <Scout.h>
 #include <ModuleHandler.h>
 
-LinkedList<PinoccioModule*> moduleList = LinkedList<PinoccioModule*>();
+LinkedList<PinoccioModule*> ModuleHandler::loadedModules;
 
 void ModuleHandler::setup() {
 }
 
 void ModuleHandler::loop() {
-  for (uint8_t i=0; i<moduleList.size(); i++) {
-    (moduleList.get(i))->loop();
+  for (uint8_t i=0; i<loadedModules.size(); i++) {
+    (loadedModules.get(i))->loop();
   }
 }
 
@@ -26,8 +26,8 @@ void ModuleHandler::list() {
 }
 
 bool ModuleHandler::loaded(char *name) {
-  for (uint8_t i=0; i<moduleList.size(); i++) {
-    if(strcmp(moduleList.get(i)->name(),name) == 0) return true;
+  for (uint8_t i=0; i<loadedModules.size(); i++) {
+    if(strcmp(loadedModules.get(i)->name(),name) == 0) return true;
   }
   
   return false;
@@ -35,21 +35,21 @@ bool ModuleHandler::loaded(char *name) {
 
 PinoccioModule *ModuleHandler::load(char *name) {
   PinoccioModule* module;
-  for (uint8_t i=0; i<moduleList.size(); i++) {
-    module = moduleList.get(i);
+  for (uint8_t i=0; i<loadedModules.size(); i++) {
+    PinoccioModule *module = loadedModules.get(i);
     if(strcmp(module->name(),name) == 0) return module;
   }
 
   module = ModulesNamed(name);
   if(module) {
-    moduleList.add(module);
+    loadedModules.add(module);
     module->setup();
   }
   return module;
 }
 
 void ModuleHandler::loaded() {
-  for (uint8_t i=0; i<moduleList.size(); i++) {
-    speol((moduleList.get(i))->name());
+  for (uint8_t i=0; i<loadedModules.size(); i++) {
+    speol((loadedModules.get(i))->name());
   }
 }
