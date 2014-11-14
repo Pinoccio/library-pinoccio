fd905a0f161dbaab26cc898769383c1674307549
Matthijs Kooijman <matthijs@stdin.nl>
2014-11-14 18:07:55 +0100

    Replace LIL REPL loop
    This just copies the serial processing from Shell.cpp, making them
    consistent and adding the arrow-up history feature to the LIL prompt.

--- examples/LIL/LIL.ino
+++ examples/LIL/LIL.ino
@@ -35,46 +35,77 @@ void setup() {
   lil = lil_new();
 
   lil_callback(lil, LIL_CALLBACK_WRITE, (lil_callback_proc_t)write);
-  Serial.print(">> ");
   lil_register(lil, "mem", (lil_func_proc_t)mem);
 }
 
-void loop() {
-  static String line;
-  static char prev = 0;
+void run_lil(const String& line) {
+  lil_value_t ret = lil_parse(lil, line.c_str(), line.length(), 1);
+  const char *msg;
+  size_t pos;
+  if (lil_error(lil, &msg, &pos)) {
+    Serial.print("Error: ");
+    Serial.println(msg);
+  } else {
+    Serial.println(lil_to_string(ret));
+  }
+  lil_free_value(ret);
+}
 
-  int c = Serial.read();
-  if (c >= 0) {
-    if (c == '\n' && prev == '\r') {
-      // Nothing
-    } else if (c == '\r' || c == '\n') {
+static StringBuffer serialIncoming;
+static String prevCommand;
+static char lastc = 0;
+static bool esc_sequence = false;
+
+void loop() {
+  while (Serial.available()) {
+    char c = Serial.read();
+
+    if (c == '\n' && lastc == '\r') {
+      // Ignore the \n in \r\n to prevent interpreting \r\n as two
+      // newlines. Note that we cannot just ignore newlines when the
+      // buffer is empty, since that doesn't allow forcing a new
+      // prompt by sending a newline (when the terminal is messed up
+      // by debug output for example).
+    } if (c == '\r' || c == '\n') {
       Serial.println();
-      if (line.length()) {
-        lil_value_t ret = lil_parse(lil, line.c_str(), line.length(), 1);
-        const char *msg;
-        size_t pos;
-        if (lil_error(lil, &msg, &pos)) {
-          Serial.print("Error: ");
-          Serial.println(msg);
-        } else {
-          Serial.println(lil_to_string(ret));
-        }
-        lil_free_value(ret);
-        line = "";
+      if (serialIncoming.length()) {
+        run_lil(serialIncoming);
+        prevCommand = serialIncoming;
+        serialIncoming = (char*)NULL;
       }
-      Serial.print(">> ");
-    } else if (c == 0x8) { // backspace
-      if (line.length()) {
-        line.remove(line.length() - 1);
-        Serial.write(0x8);
-        Serial.write(' ');
-        Serial.write(0x8);
+      Shell.prompt();
+    } else if (c == '\b') {
+      if (serialIncoming.length()) {
+        // Erase last character (backspace only moves the cursor back,
+        // so print a space to actually erase)
+        Serial.write("\b \b");
+        serialIncoming.remove(serialIncoming.length() - 1);
+      } else {
+        // Nothing to erase, send a bell
+        Serial.write('\a');
       }
+    } else if (c == '\x1b') {
+      esc_sequence = true;
+    } else if (esc_sequence && lastc == '\x1b') {
+      // Ignore the first character after the escape
+    } else if (esc_sequence && lastc != '\x1b') {
+      if (lastc == '[' && c == 'A') { // ESC[A == arrow up
+        // Erase existing command
+        Serial.write('\r');
+        for (size_t i = 0; i < serialIncoming.length(); ++i)
+          Serial.write(' ');
+        Serial.write('\r');
+        // Load and show previous command
+        Shell.prompt();
+        serialIncoming = prevCommand;
+        Serial.print(serialIncoming);
+      }
+      esc_sequence = false;
     } else {
-      line += (char)c;
-      Serial.write(c);
+      Serial.write(c); // echo everything back
+      serialIncoming += c;
     }
-    prev = c;
+    lastc = c;
   }
   Scout.loop();
 }
